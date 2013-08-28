f8769baa957149fb1a04264076d71d1668510b90
Sebastian Höffner <info@sebastian-hoeffner.de>
2013-08-28 23:37:47 +0200

    darkened image -> easier to see differences in thickness ;)

--- shader/Default1FS.glsl
+++ shader/Default1FS.glsl
@@ -18,7 +18,7 @@ void main(void)
 		discard;
 	}
 		
-	PixelColor = vec4(0.8f * (1.0f - r), 0.1f * (1.0f - r), 0, r);
+	PixelColor = 0.8f*vec4(0.8f * (1.0f - r), 0.1f * (1.0f - r), 0, r);
 	//if(alive == 0.0f) PixelColor = vec4(0.1*(1-r), 0.1*(1-r), 0.1*(1-r), r);
 	//depth = vec4(1, 0, 0, r);
 }
